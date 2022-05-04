a15a24b17674ce38a2d0468af2ded0a0ce4b75df
Anar <wiseadme@gmail.com>
2022-05-04 18:19:48 +0300

    build

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,9 +1,12 @@
 import { UnwrapRef } from 'vue'
 
 export declare type Method = (...args: any[]) => any
+
 export declare type StateTree = Record<string | number | symbol, any>
 export declare type ActionsTree = Record<string, Method>
+
 export declare type StateDefinition<S extends StateTree> = () => S
+export declare type ActionsDefinition<A> = ActionsTree extends A ? A : {}
 
 export declare type StoreOptions<S extends StateTree = {}, A = {}> = {
   state: StateDefinition<S>
@@ -16,8 +19,8 @@ export declare type Store<S extends StateTree = {}, A = {}> = {
   [key in keyof A]: A[key]
 }
 
-export declare function defineState<S extends StateTree = {}>(key: string, genState: () => S): S
+export declare function defineState<S = {}>(key: string, genState: StateDefinition<S>): S
 
-export declare function defineActions<A extends ActionsTree = {}>(key: string, actions: A): A
+export declare function defineActions<A = {}>(key: string, actions: ActionsDefinition<A>): A
 
 export declare function defineStore<S = {}, A = {}>(key: string, options?: StoreOptions<S, A>): () => Store<S, A>
