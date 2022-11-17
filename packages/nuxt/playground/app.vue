79d0b45a59858b975842f2bffcb57287ee5791bf
Anar <wiseadme@gmail.com>
2022-11-18 00:10:38 +0300

    dev: start nuxt module

---
+++ packages/nuxt/playground/app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import { useUserStore } from './store'
+
+  // @ts-ignore
+  console.log(useNervueStore(), 'store')
+
+  const store = useUserStore()
+</script>
+<template>
+  <div>
+    <h1 @click="store.setName(new Date())">Hello, {{ store.name }}</h1>
+  </div>
+</template>
